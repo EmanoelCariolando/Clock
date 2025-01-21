8b24a7a275f26aa165c957cb0568b81c24f41138
EmanoelCariolando <emanoelcariolando@gmail.com>
2025-01-20 23:54:22 -0300

    End Commit

--- script.js
+++ script.js
@@ -9,16 +9,22 @@
    let m = now.getMinutes()
    let h = now.getHours()
 
-   digitalEl.innerHTML = `${fixTime(h)}:${fixTime(m)}:${fixTime(s)}`
+   digitalEl.innerHTML = `${fixTime(h)}:${fixTime(m)}:${fixTime(s)}`;
 
-   
+   let sDeg = (360/60) * s - 90;
+   let mDeg = (360/60) * m - 90;
+   let hDeg = (360/12) * h - 90;
 
-   sElement
-
- 
+   sElement.style.transform = `rotate(${sDeg}deg)`
+   mElement.style.transform = `rotate(${mDeg}deg)`
+   hElement.style.transform = `rotate(${hDeg}deg)`
 
  }
- setInterval(uptadeClock, 1000)
+
+ setInterval(uptadeClock, 1000) 
+ uptadeClock()
+
+
  
   function fixTime (time){
   return time < 10 ?`0${time}`: time;
